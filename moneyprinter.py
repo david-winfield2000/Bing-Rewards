02517bb6da2e626340fbfe68524aea71896530f3
David Ryder <67267592+david-ryder@users.noreply.github.com>
2022-07-12 17:42:32 -0400

    shit works

---
+++ moneyprinter.py
@@ -0,0 +1,19 @@
+import webbrowser
+import string
+import random
+import os
+import time
+import winreg
+
+edge = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+
+webbrowser.register('edge', None, webbrowser.BackgroundBrowser(edge))
+
+for i in range(31):
+    letters = string.ascii_lowercase
+    query = ''.join(random.choice(letters) for i in range(10))
+    webbrowser.get('edge').open('https://www.bing.com/search?q=' + query)
+
+time.sleep(10)
+
+os.system("taskkill /im msedge.exe")
